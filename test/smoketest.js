dcf9d24ffe12c85ea6207a26570b13018578905f
Johannes <jowettinger@gmail.com>
2016-07-20 09:52:08 +0200

    tests added

--- test/smoketest.js
+++ test/smoketest.js
@@ -6,48 +6,63 @@ const fs = require('fs-extra');
 
 const prepare = require('../lib/prepare');
 
-let testDir;
+
 
 async.series([
   function(done) {
-    prepare('https://supermarket.chef.io/cookbooks/mysql/download', null, (err, workingDir, workingDirCreated) => {
+    prepare('https://supermarket.chef.io/cookbooks/mysql/download', null, (err, output) => {
       if (err) return done(err);
 
-      console.log('remote file:', workingDir, workingDirCreated);
+      console.log('remote extractable file:', output);
 
-      testDir = workingDir;
+      fs.remove(output.path, done);
+    });
+  },
+  function(done) {
+    prepare(__dirname, null, (err, output) => {
+      if (err) return done(err);
+
+      console.log('local dir:', output);
 
       done();
     });
   },
   function(done) {
-    prepare(testDir, null, (err, workingDir, workingDirCreated) => {
+    prepare(path.join(__dirname, 'fixture.zip'), null, (err, output) => {
       if (err) return done(err);
 
-      console.log('local dir:', workingDir, workingDirCreated);
+      console.log('local extractable file:', output);
+
+      fs.remove(output.path, done);
+    });
+  },
+  function(done) {
+    prepare(path.join(__dirname, 'fixture.txt'), null, (err, output) => {
+      if (err) return done(err);
+
+      console.log('local non-extractable file:', output);
 
       done();
     });
   },
   function(done) {
-    fs.remove(testDir, done);
-  },
-  function(done) {
-    prepare(path.join(__dirname, 'fixture.zip'), null, (err, workingDir, workingDirCreated) => {
+    prepare('https://gist.githubusercontent.com/jojow/0e5f6cea3d052999b662b60ad37f6d2f/raw/212ddd43f4372b95bb334aa94755412bc93cc117/opal-clus.metadata.json', null, (err, output) => {
       if (err) return done(err);
 
-      console.log('local file:', workingDir, workingDirCreated);
+      console.log('remote non-extractable file:', output);
 
-      testDir = workingDir;
-
-      done();
+      fs.remove(output.path, done);
     });
   },
   function(done) {
-    fs.remove(testDir, done);
+    prepare('https://github.com/jojow/specify-artifact.git', null, (err, output) => {
+      if (err) return done(err);
+
+      console.log('remote repository:', output);
+
+      fs.remove(output.path, done);
+    });
   }
 ], function(err) {
   if (err) throw err;
 });
-
-
