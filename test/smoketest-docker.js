0003597760982b425b3f70f409525caeefcb8729
Johannes <jowettinger@gmail.com>
2016-07-21 17:53:36 +0200

    subpaths: files in addition to subdirs supported

--- test/smoketest-docker.js
+++ test/smoketest-docker.js
@@ -38,6 +38,21 @@ async.series([
         fs.remove(result.path, done);
       });
     });
+  },
+  function(done) {
+    docker.specify('https://github.com/jojow/opentosca-dockerfiles::/winery/Dockerfile', null, (err, result) => {
+      if (err) return done(err);
+
+      console.log('artifact spec:', JSON.stringify(result.spec, null, 2));
+
+      docker.fetchDependencies(result, (err, result) => {
+        if (err) return done(err);
+
+        console.log('dependencies fetched and stored:', result.path);
+
+        fs.remove(result.path, done);
+      });
+    });
   }
 ], function(err) {
   if (err) throw err;
